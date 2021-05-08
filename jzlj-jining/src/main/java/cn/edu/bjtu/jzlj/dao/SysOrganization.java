2fc8715fa79573ba13d34905e17e6be605db18d6
Eric <903541255@qq.com>
2021-05-08 11:46:54 +0800

    用户关联修改

--- jzlj-jining/src/main/java/cn/edu/bjtu/jzlj/dao/SysOrganization.java
+++ jzlj-jining/src/main/java/cn/edu/bjtu/jzlj/dao/SysOrganization.java
@@ -85,6 +85,7 @@ public class SysOrganization implements Serializable{
     /**
      * 创建时间
      */
+    @JsonFormat( timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm:ss")
     private Date createTime;
 
     /**
@@ -95,6 +96,7 @@ public class SysOrganization implements Serializable{
     /**
      * 修改时间
      */
+    @JsonFormat( timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm:ss")
     private Date updateTime;
 
     /**
