b4753e3450cb5f9566e23bd2d6215b19fa5e8322
wangzj19 <171202898@qq.com>
2021-05-24 17:26:17 +0800

    .

---
+++ jzlj-jining/src/main/java/cn/edu/bjtu/jzlj/vo/graph/ReviewAndNumber.java
@@ -0,0 +1,15 @@
+package cn.edu.bjtu.jzlj.vo.graph;
+
+import io.swagger.models.auth.In;
+import lombok.Data;
+
+/**
+ * @author: zjwang
+ * @date: 2021/5/24 17:24
+ * @description:
+ */
+@Data
+public class ReviewAndNumber {
+    private Integer number;
+    private Integer reviewStatus;
+}
