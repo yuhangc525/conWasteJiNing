775095c08476c1a36723cc6ea0afd52b002d3462
Yingjie-tian <19120894@bjtu.edu.cn>
2021-05-25 10:33:35 +0800

    图表统计Controller加跨域注解

--- jzlj-jining/src/main/java/cn/edu/bjtu/jzlj/controller/GraphController.java
+++ jzlj-jining/src/main/java/cn/edu/bjtu/jzlj/controller/GraphController.java
@@ -22,6 +22,7 @@ import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,6 +37,7 @@ import java.util.List;
 
 @Api(description = "图表接口")
 @RestController
+@CrossOrigin
 @RequestMapping("/graph")
 public class GraphController {
 
