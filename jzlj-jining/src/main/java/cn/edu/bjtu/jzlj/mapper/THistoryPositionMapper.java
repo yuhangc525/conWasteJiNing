a2fe76345e418b750ec2bd273ea0c32e2bc0f1a1
wangzhijin <171202898@qq.com>
2021-07-30 20:47:31 +0800

    时间-区域查车，未完成...

--- jzlj-jining/src/main/java/cn/edu/bjtu/jzlj/mapper/THistoryPositionMapper.java
+++ jzlj-jining/src/main/java/cn/edu/bjtu/jzlj/mapper/THistoryPositionMapper.java
@@ -1,6 +1,7 @@
 package cn.edu.bjtu.jzlj.mapper;
 
 import cn.edu.bjtu.jzlj.dao.THistoryPosition;
+import cn.edu.bjtu.jzlj.vo.RegionalVehicleSelectionVo;
 import cn.edu.bjtu.jzlj.vo.graph.numberOfVehicleUsedVo;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
@@ -14,5 +15,14 @@ public interface THistoryPositionMapper extends BaseMapper<THistoryPosition> {
     List<THistoryPosition> getHistoryPoint(String sTime, String eTime, String terminalId, String tableName);
 
     Integer numberOfVehicleUsed(String tableName);
+
+    // 矩形区域 - 有时间范围限制查询车辆
+    List<RegionalVehicleSelectionVo> getRectangleRegionalVehiclesWithTimeLimit(double startLat, double startLong,
+            double endLat, double endLong, String startTime, String endTime, String tableName);
+
+    // 圆形区域 - 有时间范围限制查询车辆
+    List<RegionalVehicleSelectionVo> getCircleRegionalVehiclesWithTimeLimit(double centerLat, double centerLong, double semidiameter,
+            String startTime, String endTime, String tableName);
+
 
 }
